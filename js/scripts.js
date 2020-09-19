7fe7be9dcd409cf09ddea5250349e174f0dc3d2a
ken <ken@MK.local>
2020-09-19 12:22:31 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,10 @@ $(function() {
     
     $("#reserveButton").click(function(){
         $("#reserveModal").modal("show");
-    })
+    });
 
     $("#loginButton").click(function(){
         $("#loginModal").modal("show");
-    })
+    });
   
 });
